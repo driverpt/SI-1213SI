134f27549fad3d7968ae94eae4a02f3398aa1b2c
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-10-23 19:59:42 +0100

    Fixed a bug where FileVerifier didnt checked the signature based on the original file

--- src/pt/isel/adeetc/leic/FileVerifier.java
+++ src/pt/isel/adeetc/leic/FileVerifier.java
@@ -1,7 +1,6 @@
 package pt.isel.adeetc.leic;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
@@ -10,15 +9,10 @@ import java.security.Signature;
 import java.security.cert.CertPathBuilder;
 import java.security.cert.CertPathBuilderResult;
 import java.security.cert.CertStore;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.PKIXBuilderParameters;
-import java.security.cert.TrustAnchor;
 import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 
@@ -27,55 +21,58 @@ public class FileVerifier {
     /**
      * @param args
      */
-    
-	private static final String X509_TYPE = "X.509";
-	
-	private CertStore certChain;
-	private KeyStore anchors;
-	
-    public FileVerifier(CertStore certStoreChain, KeyStore anchors) {
+
+    private static final String X509_TYPE = "X.509";
+
+    private CertStore           certChain;
+    private KeyStore            anchors;
+
+    public FileVerifier( CertStore certStoreChain, KeyStore anchors ) {
         certChain = certStoreChain;
         this.anchors = anchors;
     }
 
-    public boolean verify(File signatureFile, File certificateFile) throws GeneralSecurityException {
-    	CertificateFactory certificateFactory = CertificateFactory.getInstance(X509_TYPE);
-    	X509Certificate certificate = null;
-    	try {
-			InputStream inputStream = new FileInputStream(certificateFile);
-			certificate = (X509Certificate) certificateFactory.generateCertificate(inputStream);
-			inputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-    	
-    	Signature signature = Signature.getInstance(certificate.getSigAlgName());
-    	
-    	signature.initVerify(certificate);
-    	byte[] signatureBytes = null;
-    	try {
-			signatureBytes = FileUtils.readFileToByteArray(signatureFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-    	
-    	if( !signature.verify(signatureBytes) ) {
-    		return false;
-    	}
-    	
-    	X509CertSelector selector = new X509CertSelector();
-    	selector.setCertificate(certificate);
-    	
-    	CertPathBuilder certPathBuilder = CertPathBuilder.getInstance("PKIX");
-    	
-    	PKIXBuilderParameters pkixBuilderParameters = new PKIXBuilderParameters(anchors, selector);
-    	pkixBuilderParameters.addCertStore(certChain);
-    	
-    	CertPathBuilderResult result = certPathBuilder.build(pkixBuilderParameters);
+    public boolean verify( File originalFile, File signatureFile, File certificateFile ) throws GeneralSecurityException {
+        CertificateFactory certificateFactory = CertificateFactory.getInstance( X509_TYPE );
+        X509Certificate certificate = null;
+        try {
+            InputStream inputStream = FileUtils.openInputStream( certificateFile );
+            certificate = (X509Certificate) certificateFactory.generateCertificate( inputStream );
+            inputStream.close();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+            System.exit( -1 );
+        }
 
-    	return true;
+        Signature signature = Signature.getInstance( certificate.getSigAlgName() );
+
+        signature.initVerify( certificate );
+        byte[] signatureBytes = null;
+        try {
+            byte[] originalFileBytes = FileUtils.readFileToByteArray( originalFile );
+            signature.update( originalFileBytes );
+            signatureBytes = FileUtils.readFileToByteArray( signatureFile );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+            System.exit( -1 );
+        }
+        
+        if ( !signature.verify( signatureBytes ) ) {
+            return false;
+        }
+
+        X509CertSelector selector = new X509CertSelector();
+        selector.setCertificate( certificate );
+
+        CertPathBuilder certPathBuilder = CertPathBuilder.getInstance( "PKIX" );
+        
+        PKIXBuilderParameters pkixBuilderParameters = new PKIXBuilderParameters( anchors, selector );
+        pkixBuilderParameters.addCertStore( certChain );
+        
+        // This option must be used if we're using Self-Signed Certificates
+        pkixBuilderParameters.setRevocationEnabled( false );
+
+        CertPathBuilderResult result = certPathBuilder.build( pkixBuilderParameters );
+        return true;
     }
-    
 }
